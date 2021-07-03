f7ebdcf4e1d392a3a6c2f16b228358e74015caa8
Dasha Lary <dasha4988@gmail.com>
2021-07-03 16:41:57 -0400

    created grid output

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,41 @@
 module Lib
-  ( someFunc,
-    someString,
+  ( grid,
+    languages,
+    formatGrid,
+    outputGrid,
   )
 where
 
-someFunc :: IO ()
-someFunc = putStrLn someString
+outputGrid :: [String] -> IO ()
+outputGrid grid = putStrLn (formatGrid grid)
 
-someString :: String
-someString = "someString"
+formatGrid :: [String] -> String
+formatGrid = unlines
+
+grid =
+  [ "__C________R___",
+    "__SI________U__",
+    "__HASKELL____B_",
+    "__A__A_____S__Y",
+    "__R___B___C____",
+    "__PHP____H_____",
+    "____S_LREP_____",
+    "____I__M_Y__L__",
+    "____L_E__T_O___",
+    "_________HB____",
+    "_________O_____",
+    "________CN_____"
+  ]
+
+languages =
+  [ "BASIC",
+    "COBOL",
+    "CSHARP",
+    "HASKELL",
+    "LISP",
+    "PERL",
+    "PHP",
+    "PYTHON",
+    "RUBY",
+    "SCHEME"
+  ]
