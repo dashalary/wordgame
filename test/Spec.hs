71929d6e894c5537f272334d3d7788479d5742b9
Dasha Lary <dasha4988@gmail.com>
2021-07-04 17:08:44 -0400

    added another test

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,3 +12,11 @@ main = hspec $ do
     it "Should find words that exist on the Grid" $ do
       findWord grid "HASKELL" `shouldBe` Just "HASKELL"
       findWord grid "PERL" `shouldBe` Just "PERL"
+    it "Should not find words that are not on the Grid" $ do
+      findWord grid "HAM" `shouldBe` Nothing
+
+  describe "findWords" $ do
+    it "Should find all the words that exist on the Grid" $ do
+      findWords grid languages `shouldBe` languages
+    it "Should not find words that do not exist on the Grid" $ do
+      findWords grid ["FRENCH", "GERMAN", "ENGLISH"] `shouldBe` []
