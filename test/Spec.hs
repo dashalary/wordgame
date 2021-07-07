863e3915f1a78b4ae845c77cb0ebd90bd6d99cd3
Dasha Lary <dasha4988@gmail.com>
2021-07-07 14:45:07 -0400

    refactored tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,13 @@ import Data
 import Lib
 import Test.Hspec
 
+gwc = gridWithCoords grid
+
+testFindWord word =
+  let (Just result) = findWord gwc word
+      string = map cell2char result
+   in string `shouldBe` word
+
 main :: IO ()
 main = hspec $ do
   describe "formatGrid" $ do
@@ -10,13 +17,15 @@ main = hspec $ do
 
   describe "findWord" $ do
     it "Should find words that exist on the Grid" $ do
-      findWord grid "HASKELL" `shouldBe` Just "HASKELL"
-      findWord grid "PERL" `shouldBe` Just "PERL"
+      testFindWord "HASKELL"
+      testFindWord "PERL"
     it "Should not find words that are not on the Grid" $ do
-      findWord grid "HAM" `shouldBe` Nothing
+      findWord gwc "HAM" `shouldBe` Nothing
 
   describe "findWords" $ do
     it "Should find all the words that exist on the Grid" $ do
-      findWords grid languages `shouldBe` languages
+      let found = findWords gwc languages
+          asString = map (map cell2char) found
+      asString `shouldBe` languages
     it "Should not find words that do not exist on the Grid" $ do
-      findWords grid ["FRENCH", "GERMAN", "ENGLISH"] `shouldBe` []
+      findWords gwc ["FRENCH", "GERMAN", "ENGLISH"] `shouldBe` []
